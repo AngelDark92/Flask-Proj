735c35dd629bed8a5564de4698807c1d4de79048
AngelDark92 <angelodark92@gmail.com>
2020-01-20 15:42:56 +0000

    Updated
    Starting off

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    # Todo
+    return render_template ("index.html")
 
+@app.route("/login")
+def login():
+    # Todo
+    return render_template ("login.html")
+
+@app.route("/logout")
+def logout():
+    # Todo
+    return
+
+@app.route("/register")
+def register():
+    # Todo
+    return render_template ("register.html")
+
+@app.route("/stl_viewer")
+def stl_viewer():
+    # Todo
+    return render_template ("stl_viewer.html")
+
+@app.route("/order")
+def order():
+    # Todo
+    return render_template ("order.html")
+
+@app.route("/models")
+def models():
+    # Todo
+    return render_template ("models.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
